c125112050e8f76cb270870c761ffc86861edf74
LeOzInNDF <leo_albano@hotmail.com>
2023-04-25 06:11:01 -0300

    components createds with styles

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  templateUrl: './big-card.component.html',
+  styleUrls: ['./big-card.component.css']
+})
+export class BigCardComponent implements OnInit{
+  ngOnInit(): void {
+
+  }
+
+}
---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  templateUrl: './small-card.component.html',
+  styleUrls: ['./small-card.component.css']
+})
+export class SmallCardComponent implements OnInit{
+  ngOnInit(): void {
+
+  }
+
+}
