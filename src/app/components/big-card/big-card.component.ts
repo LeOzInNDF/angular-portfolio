a6f546686a8d9758140e0678d59c27ab33af5d2d
LeOzInNDF <leo_albano@hotmail.com>
2023-04-25 09:45:20 -0300

    content created

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit{
+
+  @Input()
+  photoCover:string =""
+  @Input()
+  cardTitle:string ="NOVO HOMEM DE FERRO ANUNCIADO"
+  @Input()
+  cardDescription:string ="Marvel Studios anuncia novo homem de ferro."
+  @Input()
+  Id: string = "0"
+  constructor() {
+  }
+
   ngOnInit(): void {
 
   }
---
+++ src/app/data/datafake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Novo Homem de ferro",
+    "description": "Marvel anuncia novo homem de ferro",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "ola",
+    "photoCover":"https://legadodamarvel.com.br/wp-content/uploads/2022/02/series-originais-da-marvel-na-netflix-podem-estar-deixando-a-plataforma-legadodamarvel.jpg"
+  },
+
+
+]
