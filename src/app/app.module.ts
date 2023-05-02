7e641ee94714ebf23acc5905d4471e0f59fc1a87
LeOzInNDF <leo_albano@hotmail.com>
2023-05-02 00:10:18 -0300

    colocando projeto no GitPages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './pages/content/content.component';
-import { DarkmodeComponent } from './components/darkmode/darkmode.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 
@@ -22,7 +21,6 @@ import { FooterComponent } from './components/footer/footer.component';
     SmallCardComponent,
     HomeComponent,
     ContentComponent,
-    DarkmodeComponent,
     FooterComponent,
 
   ],
